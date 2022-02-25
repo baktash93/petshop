ce6589924d8ec92fc130292342fa7716ef5f1f2f
Ahmad Baktash Hayeri <baktash.hayeri.npa@gmail.com>
2022-02-25 22:31:47 +0430

    Adds user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,7 +18,12 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
-        'name',
+        'uuid',
+        'first_name',
+        'last_name',
+        'is_admin',
+        'avatar',
+        'is_marketing',
         'email',
         'password',
     ];
