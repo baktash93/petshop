b71660f0c97d91063bf1a3a3bffcaa52e6dee868
Ahmad Baktash Hayeri <baktash.hayeri.npa@gmail.com>
2022-02-25 23:05:29 +0430

    Set up user routes and controller

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Controller;
+
+class UserController extends Controller {
+    function show() {}
+
+    function delete() {}
+
+    function edit() {}
+
+    function login() {}
+
+    function logout() {}
+
+    function create() {}
+
+    function forgotPassword() {}
+
+    function resetPasswordToken() {}
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,22 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::group(['middleware' => [], 'prefix' => 'v1'], function () {
+    /** User endpoint */
+
+    Route::group(['prefix' => 'user'], function () {
+        Route::get('/', [UserController::class, 'show']);
+        Route::delete('/', [UserController::class, 'delete']);
+        // Route::get('/orders', [UserController::class, 'listOrders']);
+        Route::put('/edit', [UserController::class, 'edit']);
+        Route::post('/login', [UserController::class, 'login']);
+        Route::get('/logout', [UserController::class, 'logout']);
+        Route::post('/create', [UserController::class, 'create']);
+        Route::get('/forgot-password', [UserController::class, 'forgotPassword']);
+        Route::get('/reset-password-token', [UserController::class, 'resetPasswordToken']);
+    });
+});
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
