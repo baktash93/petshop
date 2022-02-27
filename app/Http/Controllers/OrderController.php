8b3edf9a82781b1feb0e753b3194d521d259a7bb
Ahmad Baktash Hayeri <baktash.hayeri.npa@gmail.com>
2022-02-27 17:44:06 +0430

    Order update, delete added

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -7,6 +7,7 @@ use App\Models\Payment;
 use App\Models\Order;
 use App\Models\OrderStatus;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class OrderController extends Controller {
     function index() {}
@@ -19,6 +20,7 @@ class OrderController extends Controller {
         try {
             $payload = $request->post();
             $payload['uuid'] = \Illuminate\Support\Str::uuid();
+            $payload['user_id'] = 1; // @todo
             $payload['order_status_id'] = OrderStatus::where('uuid', $payload['order_status_uuid'])->value('id');
             $payload['payment_id'] = Payment::where('uuid', $payload['payment_uuid'])->value('id');
             Order::create(Arr::only(
@@ -41,11 +43,47 @@ class OrderController extends Controller {
         }
     }
     
-    function show($uuid) {}
+    function show($uuid) {
+        $order = Order::where('uuid', $uuid)->first();
+        return response()->json($order, 200);
+    }
     
-    function update() {}
+    function update($uuid, Request $request) {
+        try {
+            $payload = $request->post();
+            $payload['user_id'] = 1; // @todo
+            $payload['order_status_id'] = OrderStatus::where('uuid', $payload['order_status_uuid'])->value('id');
+            $payload['payment_id'] = Payment::where('uuid', $payload['payment_uuid'])->value('id');
+            Order::where('uuid', $uuid)
+                ->update(
+                    Arr::only($request->post(), [
+                        'user_id',
+                        'order_status_id',
+                        'payment_id',
+                        'uuid',
+                        'products',
+                        'address',
+                        'delivery_fee',
+                        'amount',
+                        'shipped_at'
+                    ])
+                );
+            return response(null, 200);
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response(null, 500);
+        }
+    }
     
-    function delete() {}
+    function delete($uuid) {
+        try {
+            Order::where('uuid', $uuid)
+                ->delete();
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response(null, 500);
+        }
+    }
 
     function download() {}
 }
