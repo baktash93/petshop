35a7c9a36526285ee236cd36b49e5413090993d4
Ahmad Baktash Hayeri <baktash.hayeri.npa@gmail.com>
2022-02-27 16:38:08 +0430

    Adds laravel IDE helper generator support

--- app/Models/Payment.php
+++ app/Models/Payment.php
@@ -5,7 +5,33 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Payment
+ *
+ * @property int $id
+ * @property string $uuid
+ * @property string $type
+ * @property string $details
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\PaymentFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Payment newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Payment newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Payment query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Payment whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Payment whereDetails($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Payment whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Payment whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Payment whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Payment whereUuid($value)
+ * @mixin \Eloquent
+ */
 class Payment extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'type',
+        'details'
+    ];
 }
