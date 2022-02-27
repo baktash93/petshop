5f1c9fd4cc59e7b76215f35f5e6310450d415453
Ahmad Baktash Hayeri <baktash.hayeri.npa@gmail.com>
2022-02-27 16:58:38 +0430

    Adds model for order

---
+++ app/Models/Order.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+    
+    protected $fillable = [
+        'user_id',
+        'order_status_id',
+        'payment_id',
+        'uuid',
+        'products',
+        'address',
+        'delivery_fee',
+        'amount',
+        'shipped_at'
+    ];
+}
