6ea9418a12b876f9d3a75a94abd6ac348ceb4edf
Ahmad Baktash Hayeri <baktash.hayeri.npa@gmail.com>
2022-02-27 17:21:34 +0430

    Order creation adjustment

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -3,6 +3,10 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\Payment;
+use App\Models\Order;
+use App\Models\OrderStatus;
+use Illuminate\Http\Request;
 
 class OrderController extends Controller {
     function index() {}
@@ -11,9 +15,33 @@ class OrderController extends Controller {
 
     function dashboard() {}
 
-    function create() {}
+    function create(Request $request) {
+        try {
+            $payload = $request->post();
+            $payload['uuid'] = \Illuminate\Support\Str::uuid();
+            $payload['order_status_id'] = OrderStatus::where('uuid', $payload['order_status_uuid'])->value('id');
+            $payload['payment_id'] = Payment::where('uuid', $payload['payment_uuid'])->value('id');
+            Order::create(Arr::only(
+                $payload,
+                [
+                    'user_id',
+                    'order_status_id',
+                    'payment_id',
+                    'products',
+                    'address',
+                    'delivery_fee',
+                    'amount',
+                    'shipped_at'
+                ])
+            );
+            return response(null, 200);
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response(null, 500);
+        }
+    }
     
-    function show() {}
+    function show($uuid) {}
     
     function update() {}
     
