2ca29c2d6aa4205664c5068282a679652452ec1b
Ahmad Baktash Hayeri <baktash.hayeri.npa@gmail.com>
2022-02-28 20:17:21 +0430

    Order status list complete

--- app/Http/Controllers/OrderStatusController.php
+++ app/Http/Controllers/OrderStatusController.php
@@ -3,9 +3,32 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
+use App\Models\OrderStatus;
 
 class OrderStatusController extends Controller {
-    function index() {}
+    function index(Request $request) {
+        try {
+            $statuses = OrderStatus::whereRaw('1=1')
+                ->orderBy(
+                    empty($request->query('sortBy')) ? 'id' : $request->query('sortBy'),
+                    !empty($request->query('desc')) ? 'desc' : 'asc'
+                );
+            
+            if (!empty($request->query('page'))) {
+                $statuses->skip($request->query('page'));
+            }
+            
+            if (!empty($request->query('limit'))) {
+                $statuses->take($request->query('limit'));
+            }
+            return response()->json($statuses->get(), 200);
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response(null, 500);
+        }
+    }
 
     function create() {}
     
--- routes/api.php
+++ routes/api.php
@@ -5,6 +5,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\AccountController;
 use App\Http\Controllers\PaymentController;
+use App\Http\Controllers\OrderStatusController;
 use App\Http\Middleware\Authenticate;
 
 /*
