4286ffe5331f3ce8c20488787b1d51a67a42ca57
Ahmad Baktash Hayeri <baktash.hayeri.npa@gmail.com>
2022-03-02 17:11:31 +0430

    Adds user resource endpoint

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,11 +3,45 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Support\Arr;
+use Illuminate\Http\Request;
 
 class UserController extends Controller {
-    function show() {}
+    function show(Request $request) {
+        try {
+            $user = User::where('uuid', $request->input('user_uuid'))->first();
+            return response()->json($user, 200);
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response(null, 500);
+        }
+    }
 
-    function delete() {}
+    function delete(Request $request) {
+        try {
+            User::where('uuid', $request->input('user_uuid'))->delete();
+            return response(null, 204);
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response(null, 500);
+        }
+    }
 
-    function edit() {}
+    function edit(Request $request) {
+        try {
+            $payload = $request->post();
+            User::where('uuid', $request->input('user_uuid'))
+                ->update(Arr::only($payload, [
+                    'first_name',
+                    'last_name',
+                    'is_marketing',
+                    'phone_number'
+                ]));
+            return response(null, 204);
+        } catch (\Throwable $th) {
+            //throw $th;
+            return response(null, 500);
+        }
+    }
 }
