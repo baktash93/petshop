adce36d965a9d25ee529b8dd534056c54bd34342
Ahmad Baktash Hayeri <baktash.hayeri.npa@gmail.com>
2022-03-04 22:33:52 +0430

    IDE helper generator added for models

---
+++ _ide_helper_models.php
@@ -0,0 +1,183 @@
+<?php
+
+// @formatter:off
+/**
+ * A helper file for your Eloquent Models
+ * Copy the phpDocs from this file to the correct Model,
+ * And remove them from this file, to prevent double declarations.
+ *
+ * @author Barry vd. Heuvel <barryvdh@gmail.com>
+ */
+
+
+namespace App\Models{
+/**
+ * App\Models\JwtToken
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property string $unique_id
+ * @property string $token_title
+ * @property string|null $restrictions
+ * @property string|null $permissions
+ * @property string|null $expires_at
+ * @property string|null $last_used_at
+ * @property string|null $refreshed_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Illuminate\Database\Eloquent\Builder|JwtToken newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|JwtToken newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|JwtToken query()
+ * @method static \Illuminate\Database\Eloquent\Builder|JwtToken whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|JwtToken whereExpiresAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|JwtToken whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|JwtToken whereLastUsedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|JwtToken wherePermissions($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|JwtToken whereRefreshedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|JwtToken whereRestrictions($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|JwtToken whereTokenTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|JwtToken whereUniqueId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|JwtToken whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|JwtToken whereUserId($value)
+ */
+	class JwtToken extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Order
+ *
+ * @property int $id
+ * @property int $user_id
+ * @property int $order_status_id
+ * @property int|null $payment_id
+ * @property string $uuid
+ * @property array $products
+ * @property array $address
+ * @property float|null $delivery_fee
+ * @property float $amount
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property string|null $shipped_at
+ * @property-read \App\Models\OrderStatus $orderStatus
+ * @property-read \App\Models\Payment|null $payment
+ * @method static \Database\Factories\OrderFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Order newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Order query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereAmount($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereDeliveryFee($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereOrderStatusId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order wherePaymentId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereProducts($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereShippedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereUserId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Order whereUuid($value)
+ */
+	class Order extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\OrderStatus
+ *
+ * @property int $id
+ * @property string $uuid
+ * @property string $title
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \App\Models\Order|null $order
+ * @method static \Database\Factories\OrderStatusFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderStatus newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderStatus newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderStatus query()
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderStatus whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderStatus whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderStatus whereTitle($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderStatus whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|OrderStatus whereUuid($value)
+ */
+	class OrderStatus extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\Payment
+ *
+ * @property int $id
+ * @property string $uuid
+ * @property string $type
+ * @property string $details
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @method static \Database\Factories\PaymentFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|Payment newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Payment newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|Payment query()
+ * @method static \Illuminate\Database\Eloquent\Builder|Payment whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Payment whereDetails($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Payment whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Payment whereType($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Payment whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|Payment whereUuid($value)
+ * @mixin \Eloquent
+ * @property-read \App\Models\Order|null $order
+ */
+	class Payment extends \Eloquent {}
+}
+
+namespace App\Models{
+/**
+ * App\Models\User
+ *
+ * @property int $id
+ * @property string $uuid
+ * @property string $first_name
+ * @property string $last_name
+ * @property int $is_admin
+ * @property string $email
+ * @property \Illuminate\Support\Carbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $avatar
+ * @property string $address
+ * @property string $phone_number
+ * @property int $is_marketing
+ * @property string|null $remember_token
+ * @property string|null $last_login_at
+ * @property \Illuminate\Support\Carbon|null $created_at
+ * @property \Illuminate\Support\Carbon|null $updated_at
+ * @property-read \Illuminate\Notifications\DatabaseNotificationCollection|\Illuminate\Notifications\DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @property-read \Illuminate\Database\Eloquent\Collection|\Laravel\Sanctum\PersonalAccessToken[] $tokens
+ * @property-read int|null $tokens_count
+ * @method static \Database\Factories\UserFactory factory(...$parameters)
+ * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|User query()
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereAddress($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereAvatar($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereCreatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmail($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereEmailVerifiedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereFirstName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereIsAdmin($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereIsMarketing($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereLastLoginAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereLastName($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User wherePhoneNumber($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|User whereUuid($value)
+ * @mixin \Eloquent
+ * @property-read \Illuminate\Database\Eloquent\Collection|\App\Models\Order[] $orders
+ * @property-read int|null $orders_count
+ */
+	class User extends \Eloquent {}
+}
+
